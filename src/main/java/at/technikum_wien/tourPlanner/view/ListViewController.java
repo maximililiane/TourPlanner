b028650833a967f9cfaeb587916f02f3cb218e9a
JulianWeghaupt <if21b100@technikum-wien.at>
2022-04-14 14:20:50 +0200

    added functions for buttons in ListViewController

--- src/main/java/at/technikum_wien/tourPlanner/view/ListViewController.java
+++ src/main/java/at/technikum_wien/tourPlanner/view/ListViewController.java
@@ -1,6 +1,10 @@
 package at.technikum_wien.tourPlanner.view;
 
+import at.technikum_wien.tourPlanner.models.ListViewTour;
 import at.technikum_wien.tourPlanner.viewModel.ListViewModel;
+import javafx.event.ActionEvent;
+
+import java.util.List;
 
 public class ListViewController {
     private final ListViewModel listViewModel;
@@ -8,4 +12,27 @@ public class ListViewController {
     public ListViewController(ListViewModel listViewModel){
         this.listViewModel=listViewModel;
     }
+
+    private void addListToTable(List<ListViewTour> list){
+        for (ListViewTour t : list){
+            addTourToTable(t);
+        }
+    }
+
+    private void addTourToTable(ListViewTour t){
+        //TODO: add new row
+    }
+
+    private void showDetailsButtonPressed(ActionEvent e){
+        //TODO: show details
+    }
+
+    private void showLogsButtonPressed(ActionEvent e){
+        //TODO: show logs
+    }
+
+    private void editTourButtonPressed(ActionEvent e){
+        //TODO: edit Tour
+    }
+
 }
